cd9a0a1e18fac5383690667aedf141ecbd05b482
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-08-04 12:33:38 +0530

    theme setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,17 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Programs from './views/Components/Programs';
 import Scaler from './views/Components/Scaler';
+import { ThemeProvider } from '@mui/material';
+import {theme} from './theme';
 
 function App() {
   return (
     <>
+    <ThemeProvider theme={theme}>
       <Navbar />
       <Home />
       <Footer />
+    </ThemeProvider>
     </>
   );
 }
