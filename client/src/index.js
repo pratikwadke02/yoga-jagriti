6ae9b71ed1985e5f130dbefbd2ee50edb6328ab8
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-08-26 19:31:59 +0530

    removed redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,8 @@ import {createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { reducers } from './reducers';
-import {composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk), composeWithDevTools()));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
